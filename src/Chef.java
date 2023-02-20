1738521858b465a7f32193571d689328f46d9ab2
happybyrainbow <maximturianskyi@gmail.com>
2023-02-20 16:17:07 +0100

    p

--- src/Chef.java
+++ src/Chef.java
@@ -1,11 +1,22 @@
 public class Chef {
 
+    Personal pers1;
+    Personal pers2;
+    //boolean personalNötig = true;
+
+
     public Chef(){
+
+        pers1 = null;
+        pers2 = null;
 
     }
 
     public void neuesPersonal(Personal einP){
 
+        if(pers1 == null){
+            this.pers1 = einP;
+        }
+        this.pers2 = einP;
     }
-
 }
--- src/PointsDisplay.java
+++ src/PointsDisplay.java
@@ -1,12 +1,5 @@
 public class PointsDisplay {
 
-    private PointsDisplay irgendeinPD;
-    private Circle einNeuesC;
-
-    public PointsDisplay(){
-        irgendeinPD = new PointsDisplay();
-        einNeuesC = new Circle(irgendeinPD);
-    }
 
 
 }
